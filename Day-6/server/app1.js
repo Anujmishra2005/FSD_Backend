346fed18f73ed15d94aefe37b5e1cd4c5053d2d3
Anujmishra2005 <anujmishraan2005@gmail.com>
2025-03-04 15:32:45 +0530

    New Commit

---
+++ Day-6/server/app1.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const fd = require('fs/promises'); 
+const app = express();
+const port = 3005;
+
+let users = [];
+
+const loadUser = async () => {
+    try {
+        const data = await fd.readFile("./users.json", "utf-8"); 
+        users = JSON.parse(data);
+    } catch (err) {
+        users = [];
+    }
+};
+
+const savedata = async () => {
+    await fd.writeFile("./users.json", JSON.stringify(users, null, 2)); 
+};
+
+app.use(express.json()); // BuiltIn Middleware (Application Level Middleware)
+loadUser();
+
+app.get("/users", (req, res) => {
+    res.status(200).json(users);
+});
+
+app.get("/user/:id", (req, res) => {
+    const uid = req.params.id;
+    const index = users.findIndex(ind => ind.id == uid);
+    if (index === -1) {
+        return res.status(400).json({ status: "fail", message: "User Not Found" });
+    }
+    res.status(200).json({ status: "success", message: "User found", data: users[index] });
+});
+
+app.post("/createuser", async (req, res) => {
+    const { name, email } = req.body;
+    if (!name || !email) {
+        return res.status(400).json({ status: "fail", message: "All fields are required" });
+    }
+    const newId = Date.now();
+    const newUser = { id: newId, name, email };
+    users.push(newUser);
+    await savedata();
+    res.status(200).json({ status: "success", message: "User Created Successfully", data: newUser });
+});
+
+app.patch("/edituser/:id", async (req, res) => {
+    const uid = req.params.id;
+    const { name, email } = req.body;
+    const index = users.findIndex(ind => ind.id == uid);
+    if (index === -1) {
+        return res.status(400).json({ status: "fail", message: "User Not Found" });
+    }
+    
+    if (name) users[index].name = name;
+    if (email) users[index].email = email;
+    
+    await savedata();
+    res.status(200).json({ status: "success", message: "User Data Edited Successfully", data: users[index] });
+});
+
+app.delete("/deleteuser/:id", async (req, res) => {
+    const uid = req.params.id;
+    const index = users.findIndex(ind => ind.id == uid);
+    if (index === -1) {
+        return res.status(400).json({ status: "fail", message: "User Not Found" });
+    }
+    const deletedData = users.splice(index, 1);
+    await savedata();
+    res.status(200).json({ status: "success", message: "User Deleted Successfully", data: deletedData });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
